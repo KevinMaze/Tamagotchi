c77266785021ced995e9bc9d01b93ad3e048b062
Kévin MAZE <kekeproject@hotmail.fr>
2023-11-26 15:59:39 +0100

    add function birth

--- script.js
+++ script.js
@@ -16,6 +16,24 @@ Ses envies :
 - 💩 : caca, aléatoire minimum 30 sec et max 1.30 minutes après avoir mangé
 */
 
+/* PHASE 0 : lancer le jeu 
+1) clicker sur le bouton du milieu
+2) quand on arrive à 5 click alors on fait naitre notre tama
+*/
+const Start = () => {
+    // 1) clicker sur le bouton du milieu
+    const buttonCenter = document.querySelector('.js-button[data-direction="center"]');
+    // 2) quand on arrive à 5 click alors on fait naitre notre tama
+    let count = 0;
+    buttonCenter.addEventListener("click", () => {
+        count++;
+        console.log(count);
+        if (count === 5) {
+            birth();
+        }
+    });
+};
+
 /* PHASE 1 : naissance du tama
     1) demander le nom du personnage
     2) fait éclore l'oeuf pour passer au poussin 
@@ -23,14 +41,35 @@ Ses envies :
     4) afficher le nom du personnage dans les vitals
     5) mettre les scores des vitals à 5
  */
+const birth = () => {
+    // 1) : demander le prénom
+    const tamaName = prompt("Quel est le prénom de votre Tamastudi ?");
+    // 2) : éclore l'oeuf
+    const character = document.querySelector(".js-character");
+    character.textContent = "🐣";
+    // 3) : afficher les vitals
+    const vitals = document.querySelector(".js-vitals");
+    vitals.classList.remove("hidden");
+    // 4) : afficher le nom du personnage dans les vitals
+    const nameDisplay = document.querySelector(".js-name");
+    nameDisplay.textContent = tamaName;
+    // 5) mettre les scores des vitals à 5
+    const scoresDisplay = document.querySelectorAll(".js-score");
+    scoresDisplay.forEach((score) => {
+        score.textContent = 5;
+    });
+    // 6) : faire apparaitre les actions
+    const actions = document.querySelector(".js-actions");
+    actions.classList.remove("hidden");
+};
 
-// 1) : demander le prénom
-const tamaName = prompt("Quel est le prénom de votre Tamastudi ?");
+/* PHASE 2 : faire grandir le tama 
+1) attendre que notre tamastudi ait une première envie
+2) il devient un "grand"
+*/
+setTimeout(() => {
+    
+}, 3000);
 
-// 2) : éclore l'oeuf
-const character = document.querySelector(".js-character");
-character.textContent = "🐣";
-
-// 3) : afficher les vitals
-const vitals = document.querySelector(".js-vitals");
-vitals.classList.remove("hidden");
+// Lance la fonction de dénut de mon tama
+Start();
